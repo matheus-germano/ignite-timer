0afbbd2480b0c7d4c3c71cd24ad7d04c14513c92
Matheus Germano da Costa <dev.mgermano@gmail.com>
2022-11-21 14:15:27 -0300

    style: default theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState } from 'react'
+import { ThemeProvider } from 'styled-components'
+import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
-    <h1>Hello world</h1>
+    <ThemeProvider theme={defaultTheme}>
+
+    </ThemeProvider>
   )
 }
